3d90253e6e0f7652ce18324756df502131a3af49
Thiago Bellini <hackedbellini@async.com.br>
2014-02-27 16:31:24 -0300

    Modernize debian packaging
    * Use the new way to package python applications/library as is described here:
        https://wiki.debian.org/Python/Packaging
        https://wiki.debian.org/Python/LibraryStyleGuide
    * Correct some lintian warnings (like the missing of a homepage and the
      homepage being on the description)
    * Improve documentation on setup.py
    * Add install_requires on build.py so it's mapped right to the package
    * Remove some unneeded files
    * Add a watch file to point to pypi
    
    Change-Id: I8836c2d3eb4a9d61cfed1f09f5dc63f6bd4d489e

--- setup.py
+++ setup.py
@@ -3,43 +3,10 @@
 # Setup file for StoqDrivers
 # Code by Async Open Source <http://www.async.com.br>
 
-#
-# Dependency checking
-#
-
-dependencies = [('ZopeInterface', 'zope.interface', '3.0',
-                 'http://www.zope.org/Products/ZopeInterface',
-                 None),
-                ('kiwi', 'kiwi', (1, 9, 28),
-                 'http://www.async.com.br/projects/kiwi/',
-                 lambda x: x.kiwi_version),
-                ('PySerial', 'serial', '2.1',
-                 'http://pyserial.sourceforge.net',
-                 None)]
-for package_name, module_name, version, url, get_version in dependencies:
-    try:
-        module = __import__(module_name, {}, {}, [])
-    except ImportError:
-        raise SystemExit("The '%s' module could not be found\n"
-                         "Please install %s which can be found at %s" % (
-            module_name, package_name, url))
-
-    if not get_version:
-        continue
-
-    if version > get_version(module):
-        raise SystemExit(
-            "The '%s' module was found but it was not recent enough\n"
-            "Please install at least version %s of %s" % (
-            module_name, version, package_name))
-
-#
-# Package installation
-#
-
 from kiwi.dist import setup, listpackages, listfiles
 
 from stoqdrivers import __version__
+
 
 setup(
     name="stoqdrivers",
@@ -59,6 +26,10 @@ setup(
     ("$datadir/conf", listfiles("stoqdrivers/conf", "*.ini"))],
     global_resources=dict(conf="$datadir/conf"),
     resources=dict(locale="$prefix/share/locale"),
-    install_requires="kiwi-gtk >= 1.9.28",
+    install_requires=[
+        "kiwi-gtk >= 1.9.28",
+        "zope.interface >= 3.0",
+        "pyserial >= 2.2",
+    ]
     )
 
