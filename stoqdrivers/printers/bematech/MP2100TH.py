03154a9775a4246b2dfd0fc26be3c2fd7f6fafe8
Christian "kiko" Reis <kiko667@gmail.com>
2018-12-10 23:07:39 -0200

    Increase drawer activation time and setup printer charset
    In verifying the changes for handling inverted drawers, two additional issues
    appeared in the wild: first, the drawer activation time needs to be longer for
    at least some drawers, and second, the printer charset needs to be configured
    correctly.
    
    Fixes: https://gitlab.com/stoqtech/stoqdrivers/issues/1
    Change-Id: I3fc6d3078d579a098f94ef13d6ebd5d37a562a9d

--- stoqdrivers/printers/bematech/MP2100TH.py
+++ stoqdrivers/printers/bematech/MP2100TH.py
@@ -70,6 +70,7 @@ class MP2100TH(SerialBase):
 
         # Make sure the printer is in ESC/BEMA mode
         self.write(GS + '\xF9\x20\x00')
+        self._setup_charset()
         self.set_condensed()
         self.descentralize()
         self.unset_bold()
@@ -140,9 +141,10 @@ class MP2100TH(SerialBase):
         self.print_matrix([[False] * max_cols, [False] * max_cols, [True] * max_cols])
 
     def open_drawer(self):
-        # 5 is the number of milliseconds to activate the electrical signal. On my tests, 3 was
-        # enought on the drawer we have. 2 did not open it
-        self.write(ESC + 'v5')
+        # This is the number of milliseconds to activate the electrical signal.
+        # In tests, 3 was enough for the drawer we have. 2 did not open it. We
+        # have an example of a drawer that requires 9, so using that for now.
+        self.write(ESC + 'v9')
 
     def is_drawer_open(self):
         # b1 is to enable the printer to report the drawer status, otherwise it will report the
